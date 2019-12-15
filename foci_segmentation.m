9f9334f783b8224e6ac037100cf4bac19bcff8ae
tomasvicar <tomasvicar@gmail.com>
2019-12-15 13:48:27 +0100

    foci segmentatino running

--- foci_segmentation.m
+++ foci_segmentation.m
@@ -5,12 +5,14 @@ addpath('3DNucleiSegmentation_training')
 load('names_foci_sample.mat')
 names_orig=names;
 
-names=subdir('Y:\CELL_MUNI\foky\new_foci_detection\example_folder\*3D_*.tif');
+names=subdir('..\example_folder\*3D_*.tif');
 names={names(:).name};
 
 
 
-for img_num=3:length(names)
+for img_num=5:length(names)
+    
+    img_num
     
     name=names{img_num};
     
@@ -18,11 +20,22 @@ for img_num=3:length(names)
     
     name_mask=strrep(name,'3D_','mask_');
     
+    name_mask_foci=strrep(name,'3D_','mask_foci_');
+    
+    
+    save_control_seg=strrep(name,'3D_','control_seg');
+    save_control_seg=strrep(save_control_seg,'.tif','');
+    
+    
     [a,b,c]=read_3d_rgb_tif(name);
     
     mask=read_mask(name_mask);
     mask=split_nuclei(mask);
-    mask=balloon(mask,[26 26 10]);
+    mask=balloon(mask,[20 20 8]);
+    shape=[5,5,2];
+    [X,Y,Z] = meshgrid(linspace(-1,1,shape(1)),linspace(-1,1,shape(2)),linspace(-1,1,shape(3)));
+    sphere=sqrt(X.^2+Y.^2+Z.^2)<1;
+    mask_conected=imerode(mask,sphere);
     mask=imresize3(uint8(mask),size(a),'nearest')>0;
     
     
@@ -52,8 +65,8 @@ for img_num=3:length(names)
     
     
     ab_uint=uint8(mat2gray(ab)*255);
-    ab_percentile=prctile(ab_uint(:),0.95*100);
-    ab_uint(ab_uint<ab_percentile)=ab_percentile;
+%     ab_percentile=prctile(ab_uint(:),0.98*100);
+%     ab_uint(ab_uint<ab_percentile)=ab_percentile;
     ab_uint=ab_uint.*uint8(mask);
     
     
@@ -74,19 +87,67 @@ for img_num=3:length(names)
         M(s) = M(s) + 1;
     end
     
+    shape=[9,9,3];
+    [X,Y,Z] = meshgrid(linspace(-1,1,shape(1)),linspace(-1,1,shape(2)),linspace(-1,1,shape(3)));
+    sphere=sqrt(X.^2+Y.^2+Z.^2)<1;
+    
+    dilated=imdilate(ab_uint,sphere);
+    ab_maxima=imregionalmax(dilated);
+    
+    s = regionprops(ab_maxima>0,'Centroid');
+    maxima = round(cat(1, s.Centroid));
+    ab_maxima=false(size(ab_maxima)) ;
+    for k=1:size(maxima,1)
+        ab_maxima(maxima(k,2),maxima(k,1),maxima(k,3)) =1;
+    end
+    
+    pom=-double(ab_uint);
+    pom=imimposemin(pom,ab_maxima);
+    wab_krajeny=watershed(pom)>0;
+    wab_krajeny(M==0)=0;
+    wab_krajeny=imfill(wab_krajeny,'holes');
+    
+    
     
     toc
     
     
+    mask_2d=sum(mask,3)>0;
+    s = regionprops(mask_2d,'Centroid');
+    maxima = round(cat(1, s.Centroid));
+    mask_maxima=false([size(mask_2d,1),size(mask_2d,2)]) ;
+    for k=1:size(maxima,1)
+        mask_maxima(maxima(k,2),maxima(k,1)) =1;
+    end
+    D = bwdistgeodesic(mask_2d,mask_maxima);
+    D(isnan(D))=Inf;
+    mask_2d_split=mask_2d.*(watershed(D)>0);
+    
+    
+    
+    close all
     imshow(rgb_2d)
     hold on
-    visboundaries(sum(M,3)>0)
-    
-    
+    visboundaries(sum(wab_krajeny,3)>0,'LineWidth',0.5,'Color','r','EnhanceVisibility',0)
+    visboundaries(mask_2d_split,'LineWidth',0.5,'Color','g','EnhanceVisibility',0)
+    s = regionprops(wab_krajeny>0,'Centroid');
+    maxima = round(cat(1, s.Centroid));
+    if ~isempty(maxima)
+        plot(maxima(:,1), maxima(:,2), 'k+')
+        plot(maxima(:,1), maxima(:,2), 'yx')
+    end
+    name_orig_tmp=split(name_orig,'\');
+    name_orig_tmp=join(name_orig_tmp(end-3:end),'\');
+    title(name_orig_tmp)
     drawnow;
     
+    print(save_control_seg,'-dpng')
     
-    fdfsdfsdfdfsdf
+    
+    imwrite_binary_3D(name_mask_foci,wab_krajeny)
+    
+    
+    
     
 end
 
