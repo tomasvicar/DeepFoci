fd29df93c12a4627764763efaa32655fdc779694
tomasvicar <tomasvicar@gmail.com>
2019-12-29 17:19:34 +0100

    klicker working

--- manual_labeling.m
+++ manual_labeling.m
@@ -29,16 +29,21 @@ for img_num=1:length(names)
     save_control_seg=strrep(save_control_seg,'.tif','');
     
     
+    save_manual_label=strrep(name,'3D_','manual_label_');
+    save_manual_label=strrep(save_manual_label,'.tif','.mat');
+    
+    
+    
     [a,b,c]=read_3d_rgb_tif(name);
     
-    mask=read_mask(name_mask);
-    mask=split_nuclei(mask);
-    mask=balloon(mask,[20 20 8]);
-    shape=[5,5,2];
-    [X,Y,Z] = meshgrid(linspace(-1,1,shape(1)),linspace(-1,1,shape(2)),linspace(-1,1,shape(3)));
-    sphere=sqrt(X.^2+Y.^2+Z.^2)<1;
-    mask_conected=imerode(mask,sphere);
-    mask=imresize3(uint8(mask),size(a),'nearest')>0;
+%     mask=read_mask(name_mask);
+%     mask=split_nuclei(mask);
+%     mask=balloon(mask,[20 20 8]);
+%     shape=[5,5,2];
+%     [X,Y,Z] = meshgrid(linspace(-1,1,shape(1)),linspace(-1,1,shape(2)),linspace(-1,1,shape(3)));
+%     sphere=sqrt(X.^2+Y.^2+Z.^2)<1;
+%     mask_conected=imerode(mask,sphere);
+%     mask=imresize3(uint8(mask),size(a),'nearest')>0;
     
     
     [a,b,c]=preprocess_filters(a,b,c);
@@ -48,10 +53,13 @@ for img_num=1:length(names)
     
     mask_foci=imread(name_mask_foci);
     
+    clear labels
+    clear positions
+    semiauto_appdes(a, b, c, zeros(size(mask_foci)), mask_foci)
     
-    semiauto_appdes(a, b, c, mask, mask_foci)
+    save(save_manual_label,'labels','positions')
     
-    fdsfsdfds 
+    drawnow
     
 end
     
