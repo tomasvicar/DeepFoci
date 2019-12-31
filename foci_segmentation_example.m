1667dac5639f6280cb7c746da23238ebed27edb4
tomasvicar <tomasvicar@gmail.com>
2019-12-31 17:45:12 +0100

    split foci finished

--- foci_segmentation_example.m
+++ foci_segmentation_example.m
@@ -25,7 +25,7 @@ for img_num=2:length(names)
     name_mask_foci=strrep(name,'3D_','mask_foci_');
     
     
-    save_control_seg=strrep(name,'3D_','control_seg');
+    save_control_seg=strrep(name,'3D_','control_seg_foci');
     save_control_seg=strrep(save_control_seg,'.tif','');
     
     
@@ -50,69 +50,68 @@ for img_num=2:length(names)
     a=norm_percentile(a,0.00001);
     b=norm_percentile(b,0.00001);
     
-    %    a_percentile=prctile(a(:),0.95*100);
-    %    b_percentile=prctile(b(:),0.95*100);
-    
-    %    a(a<a_percentile)=a_percentile;
-    %    b(b<b_percentile)=b_percentile;
-    
+   
     ab=a.*b;
+    ab_uint_whole=uint8(mat2gray(ab)*255).*uint8(mask);
+    clear a b ab c
+    
+    result=zeros(size(ab_uint_whole),'uint16');
     
     
-    
-%     abb=imgaussfilt3(ab,[10,10,10/3]) ;
-%     foreground=ab>abb;
-%     
+    s = regionprops(mask>0,'BoundingBox');
+    bbs = cat(1,s.BoundingBox);
     
     
+    for cell_num =1:size(bbs,1)
+        
+        bb=bbs(cell_num,:);
+        ab_uint = ab_uint_whole(bb(2):bb(2)+bb(5),bb(1):bb(1)+bb(4),bb(3):bb(3)+bb(6));
+        
+        tic
+        %    try
+        r=vl_mser(ab_uint,'MinDiversity',0.1,...
+            'MaxVariation',0.8,...
+            'Delta',1,...
+            'MinArea', 50/ numel(ab_uint),...
+            'MaxArea',2400/ numel(ab_uint));
+        %     catch
+        %         r=[] ;
+        %     end
+
+        M = zeros(size(ab_uint),'uint16') ;
+        for x=1:length(r)
+            s = vl_erfill(ab_uint,r(x)) ;
+            M(s) = M(s) + 1;
+        end
+
+        shape=[9,9,3];
+        [X,Y,Z] = meshgrid(linspace(-1,1,shape(1)),linspace(-1,1,shape(2)),linspace(-1,1,shape(3)));
+        sphere=sqrt(X.^2+Y.^2+Z.^2)<1;
+
+        dilated=imdilate(ab_uint,sphere);
+        ab_maxima=imregionalmax(dilated);
+
+        s = regionprops(ab_maxima>0,'Centroid');
+        maxima = round(cat(1, s.Centroid));
+        ab_maxima=false(size(ab_maxima)) ;
+        for k=1:size(maxima,1)
+            ab_maxima(maxima(k,2),maxima(k,1),maxima(k,3)) =1;
+        end
+
+        pom=-double(ab_uint);
+        pom=imimposemin(pom,ab_maxima);
+        wab_krajeny=watershed(pom)>0;
+        wab_krajeny(M==0)=0;
+        wab_krajeny=imfill(wab_krajeny,'holes');
+
+        toc
+        
+        result(bb(2):bb(2)+bb(5),bb(1):bb(1)+bb(4),bb(3):bb(3)+bb(6))=wab_krajeny;
+
     
-    ab_uint=uint8(mat2gray(ab)*255);
-%     ab_percentile=prctile(ab_uint(:),0.98*100);
-%     ab_uint(ab_uint<ab_percentile)=ab_percentile;
-    ab_uint=ab_uint.*uint8(mask);
-    
-    
-    tic
-    %    try
-    r=vl_mser(ab_uint,'MinDiversity',0.1,...
-        'MaxVariation',0.8,...
-        'Delta',1,...
-        'MinArea', 50/ numel(ab),...
-        'MaxArea',2400/ numel(ab));
-    %     catch
-    %         r=[] ;
-    %     end
-    
-    M = zeros(size(ab_uint),'uint16') ;
-    for x=1:length(r)
-        s = vl_erfill(ab_uint,r(x)) ;
-        M(s) = M(s) + 1;
     end
     
-    shape=[9,9,3];
-    [X,Y,Z] = meshgrid(linspace(-1,1,shape(1)),linspace(-1,1,shape(2)),linspace(-1,1,shape(3)));
-    sphere=sqrt(X.^2+Y.^2+Z.^2)<1;
-    
-    dilated=imdilate(ab_uint,sphere);
-    ab_maxima=imregionalmax(dilated);
-    
-    s = regionprops(ab_maxima>0,'Centroid');
-    maxima = round(cat(1, s.Centroid));
-    ab_maxima=false(size(ab_maxima)) ;
-    for k=1:size(maxima,1)
-        ab_maxima(maxima(k,2),maxima(k,1),maxima(k,3)) =1;
-    end
-    
-    pom=-double(ab_uint);
-    pom=imimposemin(pom,ab_maxima);
-    wab_krajeny=watershed(pom)>0;
-    wab_krajeny(M==0)=0;
-    wab_krajeny=imfill(wab_krajeny,'holes');
-    
-    
-    
-    toc
-    
+    wab_krajeny=result;
     
     mask_2d_split1=mask_2d_split(mask,3);
     
@@ -134,9 +133,10 @@ for img_num=2:length(names)
     title(name_orig_tmp)
     drawnow;
     
+    
+    
     print(save_control_seg,'-dpng')
-    
-    
+
     imwrite_uint16_3D(name_mask_foci,wab_krajeny)
 
 
