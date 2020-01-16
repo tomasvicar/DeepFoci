6171401628d15ca94ec515ff09418672304d194e
tomasvicar <tomasvicar@gmail.com>
2020-01-16 19:51:54 +0100

    just commit

--- matReaderData.m
+++ matReaderData.m
@@ -23,8 +23,8 @@ function [window_k] = matReaderData(filename)
     end
     
 %     window_k=window_k(4:end-4,4:end-4,2:end-2,:);
-
+window_k=single(mat2gray(double(window_k),[90,600])-0.5);
 %     window_k=single(mat2gray(window_k,[90,600])-0.5);
-    window_k=single((window_k-mean(window_k(:)))/std(window_k(:))) ;
+%     window_k=single((window_k-mean(window_k(:)))/std(window_k(:))) ;
 end
 
