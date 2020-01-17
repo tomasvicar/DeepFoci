53ba73cb6f91bd207d05ab7cbc036c9c2f380293
tomasvicar <tomasvicar@gmail.com>
2020-01-17 22:36:59 +0100

    fix checkpoint

--- matReaderData_test.m
+++ matReaderData_test.m
@@ -7,8 +7,8 @@ function [window_k] = matReaderData_test(filename)
     
     
 %     window_k=window_k(4:end-4,4:end-4,2:end-2,:);
-window_k=single(mat2gray(double(window_k),[90,600])-0.5);
+% window_k=single(mat2gray(double(window_k),[90,600])-0.5);
 %     window_k=single(mat2gray(window_k,[90,600])-0.5);
-%     window_k=single((window_k-mean(window_k(:)))/std(window_k(:))) ;
+    window_k=single((window_k-mean(window_k(:)))/std(window_k(:))) ;
 end
 
