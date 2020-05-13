f9454c835a420e7164e7db246d8c8c69197823f9
tomasvicar <tomasvicar@gmail.com>
2020-05-13 23:56:31 +0200

    norm vals prepared

--- feature_extraction/feature_extraction_norm_vals.m
+++ feature_extraction/feature_extraction_norm_vals.m
@@ -38,67 +38,71 @@ for img_num=1:300
     save_features=strrep(name,'3D_','features_norm_vals_');
     save_features=strrep(save_features,'.tif','.mat');
     
-    
-    save_unet_foci_detection_data=strrep(name,'3D_','unet_foci_detection_data');
-    save_unet_foci_detection_data=strrep(save_unet_foci_detection_data,'.tif','.mat');
+
     
     [a,b,c]=read_3d_rgb_tif(name);
 
 
     [a,b,c]=preprocess_filters(a,b,c,gpu);
 
-    mask_foci=imread(name_mask_foci)>0;
+   
 
-    lbl_foci=bwlabeln(mask_foci);
     
     
-    global_mean_a=mean(a(:));
-    global_std_a=std(a(:));
-    global_mean_b=mean(b(:));
-    global_std_b=std(b(:));
-    global_mean_c=mean(c(:));
-    global_std_c=std(c(:));
     
+    perc=0.0001;
     
-    p=
+    global_a=[double(prctile(a(:),perc*100)) double(prctile(a(:),100-perc*100))];
+    global_b=[double(prctile(b(:),perc*100)) double(prctile(b(:),100-perc*100))];
+    global_c=[double(prctile(c(:),perc*100)) double(prctile(c(:),100-perc*100))];
     
-    global_pup_a=mean(a(:));
-    global_pdown_a=std(a(:));
-    global_pup_b=mean(a(:));
-    global_pdown_b=std(a(:));
-    global_pup_c=mean(a(:));
-    global_pdown_c=std(a(:));
-    
-        
+      
     mask=imread(mask_name_split);
 
-    mask_foci=imread(name_mask_foci)>0;
 
+    mask_foci=imread(name_mask_foci)>0;
     lbl_foci=bwlabeln(mask_foci);
 
      
     clear mask_foci
 
     lbl_mask=bwlabeln(mask);
+    lbl_mask=imresize3(lbl_mask,size(a),'nearest');
     
-    tmp_cell_means_a=[];
-    tmp_cell_std_a=[];
-    tmp_cell_means_b=[];
-    tmp_cell_std_b=[];
-    tmp_cell_means_c=[];
-    tmp_cell_std_c=[];
-    for k=1:max(lbl_mask)
-        tmp_cell_means_a=[tmp_cell_means_a];
-        tmp_cell_std_a=[tmp_cell_std_a];
-        tmp_cell_means_b=[tmp_cell_means_b];
-        tmp_cell_std_b=[tmp_cell_std_b];
-        tmp_cell_means_c=[tmp_cell_means_c];
-        tmp_cell_std_c=[tmp_cell_std_c];
+    tmp_cell_a={};
+    tmp_cell_b={};
+    tmp_cell_c={};
+    
+    perc=0.0001*0.01;
+    
+    N=max(lbl_mask(:));
+    for k=1:N
+        tmp_cell_a=[tmp_cell_a, [double(prctile(a(lbl_mask==k),perc*100)) double(prctile(a(lbl_mask==k),100-perc*100))]];
+        tmp_cell_b=[tmp_cell_b, [double(prctile(b(lbl_mask==k),perc*100)) double(prctile(b(lbl_mask==k),100-perc*100))]];
+        tmp_cell_c=[tmp_cell_c, [double(prctile(c(lbl_mask==k),perc*100)) double(prctile(c(lbl_mask==k),100-perc*100))]];
     end
     norm_vals = regionprops3(lbl_foci,lbl_mask,'MaxIntensity');
-    norm_vas.Properties.VariableNames={'cellNum'};
+    norm_vals.Properties.VariableNames={'cellNum'};
+    cell_nums=[norm_vals{:,:}];
     
-    cell_num
+    global_a=repmat({global_a},[length(cell_nums),1]);
+    global_b=repmat({global_b},[length(cell_nums),1]);
+    global_c=repmat({global_c},[length(cell_nums),1]);
     
+    norm_vals = addvars(norm_vals,global_a,'NewVariableNames','globalA');
+    norm_vals = addvars(norm_vals,global_b,'NewVariableNames','globalB');
+    norm_vals = addvars(norm_vals,global_c,'NewVariableNames','globalC');
+    
+    tmp=[tmp_cell_a(cell_nums)]';
+    norm_vals = addvars(norm_vals,tmp,'NewVariableNames','cellA');
+    tmp=[tmp_cell_b(cell_nums)]';
+    norm_vals = addvars(norm_vals,tmp,'NewVariableNames','cellB');
+    tmp=[tmp_cell_c(cell_nums)]';
+    norm_vals = addvars(norm_vals,tmp,'NewVariableNames','cellC');
+    
+    
+    save(save_features,'norm_vals')
+    
+    drawnow;
 end
     
