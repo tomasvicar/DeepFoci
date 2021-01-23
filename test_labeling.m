542d27393c1a92ba5b0d45ca14ebeeab904c8c26
Vičar Tomáš (147472) <vicar@vutbr.cz>
2021-01-23 10:15:35 +0100

    QDF read working

--- test_labeling.m
+++ test_labeling.m
@@ -1,7 +1,7 @@
 clc;clear all;close all;
 addpath('utils')
 
-path = 'G:\Sdílené disky\martin_data\NHDF_preprocess\NHDF_8h PI\IR 0,5Gy_8h PI';
+path = 'G:\Sdílené disky\martin_data\NHDF_preprocess\NHDF_30min PI\IR 0,5Gy_30min PI';
 
 
 file_names = subdir([path '/cell*.mat']);
@@ -9,23 +9,28 @@ file_names = subdir([path '/cell*.mat']);
 
 for file_num = 1%:length(file_names)
     
-    load(file_names(file_num).name)
+    tmp = load(file_names(file_num).name);
     
-    img = img_crop(:,:,:,1);
+    img = tmp.img_crop(:,:,:,1);
+    mask = tmp.mask_crop(:,:,:,1);
     
-    M = robust_3d_maxima_detector(img,[9 9 3],0,prctile(img(:),98));
+
+    M = robust_3d_maxima_detector(img,[9 9 3],prctile(img(mask),98)-prctile(img(mask),80),prctile(img(mask),98));
     
-     imshow(max(M,[],3),[])
+    figure;
+    imshow(max(M,[],3),[])
+    figure
+    imshow(max(img,[],3),[])
     
+    drawnow;
     
-    
-    imshow(max(imregionalmax(img_crop(:,:,:,1)),[],3),[])
-    
-    imshow(imregionalmax(img_crop(:,:,:,1)),[])
-    
-    imshow( mat2gray(squeeze(max(img_crop,[],3))))
-    
-    imshow( mat2gray(squeeze(max(img_crop(:,:,:,1),[],3))))
+%     imshow(max(imregionalmax(img_crop(:,:,:,1)),[],3),[])
+%     
+%     imshow(imregionalmax(img_crop(:,:,:,1)),[])
+%     
+%     imshow( mat2gray(squeeze(max(img_crop,[],3))))
+%     
+%     imshow( mat2gray(squeeze(max(img_crop(:,:,:,1),[],3))))
     
     
 end
