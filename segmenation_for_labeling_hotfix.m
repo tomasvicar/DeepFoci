5f2c7000acb8b82ba48adc45f07ca850b93e1588
tomasvicar <tomasvicar@gmail.com>
2021-02-08 23:22:24 +0100

    hotfix2

--- segmenation_for_labeling_hotfix.m
+++ segmenation_for_labeling_hotfix.m
@@ -47,7 +47,7 @@ for file_num = 1:length(file_names)
 %     imwrite_uint16_3D(save_name,mask)
     
     save_name = replace(replace(file_name,data_path,save_path),'01.ics','mask.tif');
-    mask=read_mask(name);
+    mask=read_mask(save_name);
 
     
     bbs = regionprops3(mask,'BoundingBox');
