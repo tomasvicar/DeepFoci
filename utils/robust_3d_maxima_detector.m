a3064e25f9b05f3ead15a7725e0de4bfc1224971
Vičar Tomáš (147472) <vicar@vutbr.cz>
2021-02-08 20:42:49 +0100

    in progress

--- utils/robust_3d_maxima_detector.m
+++ utils/robust_3d_maxima_detector.m
@@ -1,12 +1,18 @@
-function [c,v] = robust_3d_maxima_detector(img,dil,h)
+function [c,v] = robust_3d_maxima_detector(img,dil,h,t)
 
     img1 = imdilate(img,sphere(dil));
     img1 = imhmax(img1,h);
     M = imregionalmax(img1);
     c = regionprops(M,'centroid');
     c = round(cat(1, c.Centroid));
-    v = img(sub2ind(size(M),c(:,2),c(:,1),c(:,3)));
     
+    
+    
+    v = img(sub2ind(size(M),c(:,2),c(:,1),c(:,3)));
+    use = v>t;
+    
+    v = v(use);
+    c = c(use,:);
 
 
 end
