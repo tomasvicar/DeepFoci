4b8f894e92f63443166c52a158db96d6f3393073
Vičar Tomáš (147472) <vicar@vutbr.cz>
2021-02-09 17:31:39 +0100

    prvni verze martinovi

--- semiauto_klicker/robust_3d_maxima_detector.m
+++ semiauto_klicker/robust_3d_maxima_detector.m
@@ -3,6 +3,7 @@ function [c,v] = robust_3d_maxima_detector(img,mask,dil,h,t)
     img1 = imdilate(img,sphere(dil));
     img1 = imhmax(img1,h);
     M = imregionalmax(img1);
+    
     M(mask==0)=0;
     c = regionprops(M,'centroid');
     c = round(cat(1, c.Centroid));
