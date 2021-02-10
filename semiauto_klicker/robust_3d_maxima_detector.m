74391abbcc68f349bd7426ddcd8ab4eddfdee2ca
Vičar Tomáš (147472) <vicar@vutbr.cz>
2021-02-10 13:19:29 +0100

    verze2 hotova

--- semiauto_klicker/robust_3d_maxima_detector.m
+++ semiauto_klicker/robust_3d_maxima_detector.m
@@ -1,4 +1,4 @@
-function [c,v] = robust_3d_maxima_detector(img,mask,dil,h,t)
+function [c,v] = robust_3d_maxima_detector(img,mask,dil,h)
 
     img1 = imdilate(img,sphere_gui(dil));
 
@@ -13,10 +13,6 @@ function [c,v] = robust_3d_maxima_detector(img,mask,dil,h,t)
     
     
     v = img(sub2ind(size(M),c(:,2),c(:,1),c(:,3)));
-    use = v>t;
-    
-    v = v(use);
-    c = c(use,:);
 
 
 end
