3367531bd93cd1bbd55c6eb91bcbde18e25a9b2d
Vičar Tomáš (147472) <vicar@vutbr.cz>
2021-03-16 23:16:28 +0100

    xxx

--- semiauto_klicker/robust_3d_maxima_detector.m
+++ semiauto_klicker/robust_3d_maxima_detector.m
@@ -10,8 +10,6 @@ function [c,v] = robust_3d_maxima_detector(img,mask,dil,h)
     c = regionprops(M,'centroid');
     c = round(cat(1, c.Centroid));
     
-    
-    
     v = img(sub2ind(size(M),c(:,2),c(:,1),c(:,3)));
 
 
