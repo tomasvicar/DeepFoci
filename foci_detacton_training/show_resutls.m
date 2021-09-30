e3256bc24dae96f627417eaf72e28336054efc4d
Vičar Tomáš (147472) <vicar@vutbr.cz>
2021-09-30 08:45:49 +0200

    ab comparison

--- foci_detacton_training/show_resutls.m
+++ foci_detacton_training/show_resutls.m
@@ -44,10 +44,79 @@ for fold = 1:5
     
     results_folder_actual = [resutls_folder '/' reutls_name '_' num2str(fold)];
     
+    res_a = load([results_folder_actual '/resutls_a.mat' ]);
+    res_b = load([results_folder_actual '/resutls_b.mat' ]);
+    res_ab = load([results_folder_actual '/resutls_ab.mat' ]);
     
+    ab_gts = res_ab.gt_points;
+    a_res = res_a.results_points;
+    b_res = res_b.results_points;
     
+    dices = [];
+    for k = 1:length(ab_gts)
+        
+        gt_points = ab_gts{k};
+        
+        a = a_res{k};
+        b = b_res{k};
+        
+        
+        factor = 2;
+        d_t = 10;
+        
+        r=a(:,1);
+        c=a(:,2);
+        v=a(:,3);
+        v = v * factor;
+        pos1 = [r,c,v];
+        
+        r=b(:,1);
+        c=b(:,2);
+        v=b(:,3);
+        v = v * factor;
+        pos2 = [r,c,v];
+        
+
+        D = pdist2(pos1,pos2);
+        D(D>d_t)=Inf;
+                
+       
+        [assignment,cost]=munkres(D);
+
+        new_points = [];
+        for ass_ind = 1:length(assignment)
+            ass = assignment(ass_ind);
+            if ass ==0
+                continue; 
+            end
+
+            new_point = int32((pos1(ass_ind,:) + pos2(ass,:))/2);
+
+            new_point(3) = int32(round(new_point(3)/factor));
+
+
+            new_points = [new_points;new_point];
+
+        end
+        
+        
+        res_points = new_points;
+        
+        
+        
+        dice = dice_points(gt_points,res_points);
+        
+        dices = [dices,dice];
+    end
     
+    dice = mean(dices);
     
     res_all.ab_sep = [res_all.ab_sep dice];
     
 end
+
+
+
+
+
+
